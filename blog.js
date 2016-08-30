c01cf7821aaff4d71740ddb6f46d94e220de0e1e
Geri Peikova <gergana.peikova@gmail.com>
2016-08-30 11:21:37 +0300

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,9 @@
+function outputquestion(){
+    var x,y,question,a,b,answer;
+    x=document.getElementById("form1");
+    y=x.elements["question"].value;
+
+    if() {document.getElementById("demo").innerHTML="You enetered this successfully";}
+    else{document.getElementById("demo").innerHTML="You're wrong";
+    }
+}
