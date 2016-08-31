3a97ee2cd8a6a8ca56a8fea98af196cfd3836fda
Geri Peikova <gergana.peikova@gmail.com>
2016-08-31 22:03:02 +0300

    Add files via upload

--- blog.js
+++ blog.js
@@ -1,3 +1,7 @@
+const kinveyAppID = 'kid_ryZ3D2fo';
+const kinveyAppSecret = 'eebed01d81c64ee1b77b3b850af1b650';
+const kinveyServiceBaseUrl = 'https://baas.kinvey.com';
+
 function outputquestion(){
     var x,y,question,a,b,answer;
     x=document.getElementById("form1");
